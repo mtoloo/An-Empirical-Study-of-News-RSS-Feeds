cb2b8741e81fdc31cfff074a729a6537c307d27f
toloo <toloo@neginmail.ir>
2016-10-25 10:05:48 +0330

    Separate config and source sites Add more sites

--- config.py
+++ config.py
@@ -8,21 +8,3 @@ MONGO_HOST = "localhost"
 MONGO_PORT = 27017
 MONGO_DATABASE = "newsportal"
 
-NEWS_SITES = {
-    'news18-india': 'http://www.news18.com/rss/india.xml',
-    'news18-buzz': 'http://www.news18.com/rss/buzz.xml',
-    'news18-lifestyle': 'http://www.news18.com/rss/lifestyle.xml',
-    'news18-south-cinema': 'http://www.news18.com/rss/south-cinema.xml',
-    'news18-world': 'http://www.news18.com/rss/world.xml',
-    'news18-formula-one': 'http://www.news18.com/rss/formula-one.xml',
-    'news18-reviews': 'http://www.news18.com/rss/reviews.xml',
-    'news18-books': 'http://www.news18.com/rss/books.xml',
-    'news18-hockey': 'http://www.news18.com/rss/hockey.xml',
-    'news18-politics': 'http://www.news18.com/rss/politics.xml',
-    'news18-tech': 'http://www.news18.com/rss/tech.xml',
-    'news18-business': 'http://www.news18.com/rss/business.xml',
-    'google-main': 'https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&output=rss',
-    'google-technology': 'http://news.google.com/news?cf=all&hl=en&pz=1&ned=us&topic=tc&output=rss',
-    'google-science': 'http://news.google.com/news?cf=all&hl=en&pz=1&ned=us&topic=snc&output=rss',
-    'google-health': 'http://news.google.com/news?cf=all&hl=en&pz=1&ned=us&topic=m&output=rss',
-}
